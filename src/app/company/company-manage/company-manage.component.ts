d349e4dba54ca06ff3c15ad6d92c4a09b62a42f7
keyur054 <keyur054@users.noreply.github.com>
2019-12-28 17:24:12 +0530

    manage company half

--- src/app/company/company-manage/company-manage.component.ts
+++ src/app/company/company-manage/company-manage.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyManageComponent implements OnInit {
 
+  companydata: any = {
+    active: true
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
